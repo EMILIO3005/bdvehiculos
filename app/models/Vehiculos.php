b42bac7925b2ea6096028c68ecb1b903f5e46548
Emilio <emiliocueva30@gmail.com>
2025-12-28 12:00:13 -0500

    Avance del proyecto

--- app/models/Vehiculos.php
+++ app/models/Vehiculos.php
@@ -32,4 +32,97 @@ class Vehiculos extends Conexion{
     }
       
   }
+
+  public function registrar($registro = []){
+    try{
+      $sql = " 
+      INSERT INTO vehiculos
+        (placa, marca, gama, modelo, fechaFabricacion, color, tipo_combustible, precio) VALUES
+        (?,?,?,?,?,?,?,?)
+        
+        ";
+
+        $consulta = $this->pdo->prepare($sql);
+        $consulta->execute(
+          array(
+           $registro['placa'],
+           $registro['marca'],
+           $registro['gama'],
+           $registro['modelo'],
+           $registro['fechaFabricacion'],
+           $registro['color'],
+           $registro['tipo_combustible'],
+           $registro['precio']
+         ));
+
+         return $this->pdo->lastInsertId();
+    }
+    catch(Exception $e){
+      return -1;
+    }
+  }
+
+
+  public function eliminar($id){
+    try{
+      $sql = "DELETE FROM vehiculos WHERE id = ?";
+      $consulta = $this->pdo->prepare($sql);
+      $consulta->execute(array($id));
+
+      return $consulta->rowCount();
+    }
+    catch(Exception $e){
+      return -1;
+    }
+  }
+
+
+  public function actualizar($registro =[]){
+    try{
+      $sql = "
+      UPDATE vehiculos SET
+      placa = ?,
+      marca = ?,
+      gama = ?,
+      modelo = ?,
+      fechafabricacion = ?,
+      color = ?,
+      tipo_combustible = ?,
+      precio = ?
+      WHERE id = ?
+      ";
+
+      $consulta = $this->pdo->prepare($sql);
+      $consulta->execute(
+          array(
+         $registro['placa'],
+         $registro['marca'],
+         $registro['gama'],
+         $registro['modelo'],
+         $registro['fechaFabricacion'],
+         $registro['color'],
+         $registro['tipo_combustible'],
+         $registro['precio'],
+         $registro['id']
+      ));
+
+        return $consulta->rowCount();
+    }
+    catch(Exception $e){
+      return -1;
+    }
+  }
+
+  public function buscarid($id){
+    try{
+      $sql = "SELECT * FROM vehiculos WHERE id = ?";
+      $consulta = $this->pdo->prepare($sql);
+      $consulta->execute(array($id));
+
+      return $consulta->fetchAll(PDO::FETCH_ASSOC);
+    }
+    catch(Exception $e){
+      return [];
+    }
+  }
 } 
---
+++ views/vehiculos/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Vehiculos</title>
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+  <div class="container">  
+    <div>
+      <h3>Mantenimiento de vehiculos</h3>
+      <p>Este modulo permite el listado, eliminacion, edicion</p>
+    </div>
+    <hr>
+    <div class="table-responsive">
+      <table class="table table-sm table-striped">
+        <thead>
+          <tr>
+            <th>Placa</th>
+            <th>Marca</th>
+            <th>Gama</th>
+            <th>Modelo</th>
+            <th>Fecha</th>
+            <th>Color</th>
+            <th>Combustible</th>
+            <th>Precio</th>
+            <th>Aciones</th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  </div>
+</body>
+</html>
